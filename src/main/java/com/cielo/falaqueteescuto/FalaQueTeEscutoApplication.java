92344582f4ad3cb3784187ba1235e0322fa6fedc
Iury Rodrigues Castro <iurycatro@MacBook-Air-de-Iury.local>
2023-09-19 09:43:22 -0300

    Initial commit

---
+++ src/main/java/com/cielo/falaqueteescuto/FalaQueTeEscutoApplication.java
@@ -0,0 +1,13 @@
+package com.cielo.falaqueteescuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FalaQueTeEscutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FalaQueTeEscutoApplication.class, args);
+	}
+
+}
